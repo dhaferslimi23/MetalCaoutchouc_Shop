fbc77724e17dab5b237314aa496bdc592f4eb2b9
Dhafer <dhaferslimi81@gmail.com>
2025-02-16 01:21:33 +0100

    Updates

---
+++ precache-manifest.66d89f4133d49d64b412f3e57917027d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c3680d22c3639eb837b4aa27af6661",
+    "url": "http://localhost:3001/index.html"
+  },
+  {
+    "revision": "46911e7db4f15eee6f3b",
+    "url": "http://localhost:3001/static/css/main.9be393e3.chunk.css"
+  },
+  {
+    "revision": "2e0acf852c7e15a9d2f6",
+    "url": "http://localhost:3001/static/js/2.a0a9e95a.chunk.js"
+  },
+  {
+    "revision": "490a723a343610728eae6b6b3ca4855a",
+    "url": "http://localhost:3001/static/js/2.a0a9e95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46911e7db4f15eee6f3b",
+    "url": "http://localhost:3001/static/js/main.836e263e.chunk.js"
+  },
+  {
+    "revision": "46f5839ad28442b4afd9",
+    "url": "http://localhost:3001/static/js/runtime-main.bc5b59c6.js"
+  },
+  {
+    "revision": "4672c2c05c1668c2b02cb489067569b2",
+    "url": "http://localhost:3001/static/media/video.4672c2c0.mp4"
+  }
+]);
